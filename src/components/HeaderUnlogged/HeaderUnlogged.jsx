0df75532c3a0461c2258b63b78f109b2454c4c8a
TeDdi-pixel <2003valuyga2003@gmail.com>
2023-08-24 15:38:07 +0300

    first commit

---
+++ src/components/HeaderUnlogged/HeaderUnlogged.jsx
@@ -0,0 +1,21 @@
+function HeaderUnlogged() {
+    return (
+        <header className="header">
+            <nav className="header__nav">
+                <div className="header-left-side">
+                    <div className="header-find-flights">
+                        <ul>
+                            <li>dasdasdasd</li>
+                            <li>fsasdas</li>
+                        </ul>
+                    </div>
+                    <div className="header-find-stays">
+                        
+                    </div>
+                </div>
+            </nav>
+        </header>
+    );
+}
+
+export default HeaderUnlogged;
